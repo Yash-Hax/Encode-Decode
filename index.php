df4aa708700ce3881ec6acaf07e05d966507bf4c
Yash Shendge <75063323+Yash-Hax@users.noreply.github.com>
2022-04-12 11:15:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+		<link rel="stylesheet" href="index.css">
+	</head>
+<body>
+	<h1>Encode/Decode tools</h1>
+	<!-- list -->
+ 	<ul>
+    <a href="base64">
+			<li style="--accent-color:#60c5a6">
+				<div class="icon"><img src="https://img.icons8.com/ios/50/000000/base-64--v2.png"/></div>
+				<div class="title">Base64</div>
+				<div class="descr">Encode/Decode Base64 form</div>
+			</li>
+		</a>
+		<a href="url">
+			<li style="--accent-color:#f20071">
+				<div class="icon"><img src="https://img.icons8.com/office/40/000000/link.png"/></i></div>
+				<div class="title">Url Encoding</div>
+				<div class="descr">URl Encode/Decode.</div>
+			</li>
+		</a> 
+	</ul>
+	<!-- list closed -->
+</body>
+</html>
---
+++ url/index.php
@@ -0,0 +1,67 @@
+<!-- Developed by Yash Hax  -->
+<!-- Twitter: @yash_hax -->
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    <title>URL Enconding</title>
+  </head>
+  <body>
+  <!-- PHP error hide & Post capture -->
+  <?php error_reporting(0); ini_set('display_errors', 0);?>
+  <?php $encode=$_POST['encode']; ?>
+  <?php $decode=$_POST['decode']; ?>
+  <!-- PHP error hide & Post capture Closed-->
+
+  <!-- Heading & Logo-->
+  <div Class="heading my-4">
+    <h1 class="base64">URL <span  class="en">Encode/Decode</span></h1>
+  </div>
+  <div class="heading mb-4">
+    <h2 class="base64">Make it <span  class="simple">simple!</span></</h2>
+  </div>
+  <!-- Heading Closed -->
+  <!-- Input OutPut encode-->
+	<div class="container">
+    <div class="row">
+    	<div class="col">
+      	<form action="index.php" method="post">
+        	<h1>Encode</h1>
+         		<br>
+        	<input name="encode" id="myInput" value="<?php echo htmlspecialchars($encode)?>" class="eninput">
+      	</form> 
+      </div>
+      <div class="col">
+      	<h1>Output</h1>
+					<br>
+				<Textarea class="enoutput"><?php echo urlencode($encode);?></Textarea>
+  	</div>
+  </div>
+  <!-- Input OutPut encode closed-->
+  <!-- Input OutPut decode closed-->
+	<div class="container my-4 ">
+  	<div class="row">
+    	<div class="col">
+    		<form action="index.php" method="post">
+    			<h1>Decode</h1>
+						<br>
+					<input name="decode"  value="<?php echo htmlspecialchars($decode);?>"  class="deinput">
+			  </form>
+   		</div>
+    	<div class="col">     
+    		<h1>Output</h1>
+					<br>
+				<Textarea class="deoutput"><?php echo htmlspecialchars(urldecode($decode));?> </Textarea>
+      </div>
+    </div>
+  </div>	
+  <!-- Input OutPut decode closed-->
+  <!-- bootstrap JS -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <!-- bootstrap JS Closed -->
+  </body>
+</html>
